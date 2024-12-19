04982abc9aaf28d62a4707324315a25c348806b9
JLRWOLF44 <jeanlouisroger44@gmail.com>
2024-12-19 11:38:54 +0100

    creation of the different components and import

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,16 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <img
+          src="https://img.freepik.com/vecteurs-libre/degrade-illustration-oiseau-colore_343694-1741.jpg?t=st=1734345512~exp=1734349112~hmac=f4e238904694de9776157a90fbbe90f3269ffb606914d81e15af57f599aba2b1&w=740"
+          alt=" logo"
+        />
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
